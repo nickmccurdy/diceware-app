3978fd08c62b89214d79e900bacfc4e227f3d2b3
Nick McCurdy <thenickperson@gmail.com>
2016-12-21 19:50:34 -0500

    Extract getWordlist()

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ var send = require('koa-send')
 
 var app = new Koa()
 
+function getWordlist () {
+  return fetch('https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt')
+    .then(response => response.text())
+}
+
 app.use(context => {
   if (context.path === '/wordlist') {
-    return fetch('https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt')
-      .then(response => response.text())
-      .then(result => { context.body = result })
+    return getWordlist().then(result => { context.body = result })
   } else {
     return send(context, 'index.html')
   }
